08f1aea160847fd9873f36a93dedd2433c034b39
Ruslan_Sharapov <Enzyme@bk.ru>
2022-10-26 11:18:30 +0300

    Написать программу копирования массива

---
+++ work 3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Написать программу копирования массива
+
+int[] array = new int[10];
+Console.Write("Original array: ");
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 10);
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+
+int arrayCopy;
+Console.Write("Copy array: ");
+
+for (int i = 0; i < array.Length; i++)
+{
+    arrayCopy = array[i];
+    Console.Write($"{arrayCopy} ");
+}
